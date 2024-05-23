cacc19fcc64aead9692af069e874455358b8a976
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2024-05-23 18:08:38 +0400

    build for vercel

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
-
-export const mongoDBURL =
-  "mongodb+srv://root:root@book-store-cluster.6bdb34u.mongodb.net/?retryWrites=true&w=majority&appName=book-store-cluster";
+import dotenv from "dotenv";
+dotenv.config();
+export const mongoDBURL = process.env.MONGO_URL;
